4edaa25b829c8adc751fba18c07d9401f8698fa7
Lydia Vo <avengerd13@gmail.com>
2025-01-12 22:36:44 -0500

    Update Assignment_01.py
    Comment

--- Assignment_01/Assignment_01.py
+++ Assignment_01/Assignment_01.py
@@ -4,9 +4,8 @@ Assignment 1 Magic Trick
 01/12/2025
 """
 
-from datetime import datetime
+
 try:
-        current_year = datetime.now().year
         number = int(input("Pick a number between 1 and 9: "))
         
         result = number * 2
@@ -15,15 +14,16 @@ try:
         
         result *= 50
         
-        result += current_year - 253
+        result += 1749
         
-        result += 2
+        result += 25
         
         result -= 2002
     
         print(f"Result is: {result}")
     
         orginal_number = result // 100
+        #The orginal
         age = result % 100
         
         print(f"The orignal number is {orginal_number}")
